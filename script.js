ca2539f6c5522d5922fdb9d44b43b385afac4d0b
tanookibruv <erendon1836@gmail.com>
2022-06-11 22:07:55 -0700

    uploading progress to git

---
+++ script.js
@@ -0,0 +1,7 @@
+var weatherAPIUrl = 'https://api.openweathermap.org';
+var weatherAPIKey = 'c0d5f49124cf4cdbd7436b6982c9b2fe';
+
+var searchFormEl = document.querySelector('#search-form');
+var searchInputEl = document.querySelector('#search-input');
+var todayEl = document.querySelector('#daily');
+
